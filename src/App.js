80d8dfd6b88f99b3bed3aefc0c5c5c90264e957b
sam <samscloud2@gmail.com>
2023-03-15 13:28:08 -0300

    resolve confict

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Heading } from './FirstComp';
 function App() {
-<<<<<<< HEAD
-=======
   return <Heading />;
->>>>>>> main
+
 }
 
 export default App;
